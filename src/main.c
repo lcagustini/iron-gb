8b7b6f3858ff3f5861a72d5c47cdf639cb40cc97
lcagustini <torresmon235@gmail.com>
2020-01-25 15:26:06 -0300

    Home button works

--- src/main.c
+++ src/main.c
@@ -140,6 +140,41 @@ bool debug = false;
 #include "mmu.c"
 #include "cpu.c"
 
+/* Exit callback */
+int exit_callback(int arg1, int arg2, void *common)
+{
+	sceKernelExitGame();
+
+	return 0;
+}
+
+/* Callback thread */
+int CallbackThread(SceSize args, void *argp)
+{
+	int cbid;
+
+	cbid = sceKernelCreateCallback("Exit Callback", exit_callback, NULL);
+	sceKernelRegisterExitCallback(cbid);
+
+	sceKernelSleepThreadCB();
+
+	return 0;
+}
+
+/* Sets up the callback thread and returns its thread id */
+int SetupCallbacks(void)
+{
+	int thid = 0;
+
+	thid = sceKernelCreateThread("update_thread", CallbackThread, 0x11, 0xFA0, THREAD_ATTR_USER, 0);
+	if(thid >= 0)
+	{
+		sceKernelStartThread(thid, 0, 0);
+	}
+
+	return thid;
+}
+
 void DMGReset() {
   fread(&ram, 0x8000, 1, cartridge.rom);
 
@@ -414,6 +449,8 @@ void loadCartridge(char *path) {
 }
 
 int main(int argc, char* argv[]) {
+	SetupCallbacks();
+
   SDL_Window *window = NULL;
 
   if(SDL_Init(SDL_INIT_VIDEO) < 0){
